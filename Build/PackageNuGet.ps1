f2f4ead04bac1f1519614600eded894ad9015884
Ben Golden <bgolden@awareweb.com>
2016-01-09 16:42:26 -0600

    add VisualStudioVersion override prop to nuget pack command

--- Build/PackageNuGet.ps1
+++ Build/PackageNuGet.ps1
@@ -2,11 +2,12 @@ param($scriptRoot)
 
 $ErrorActionPreference = "Stop"
 $programFilesx86 = ${Env:ProgramFiles(x86)}
-$msBuild = "$programFilesx86\MSBuild\14.0\bin\msbuild.exe"
+$vsVersion = "14.0"
+$msBuild = "$programFilesx86\MSBuild\$vsVersion\bin\msbuild.exe"
 $nuGet = "$scriptRoot..\tools\NuGet.exe"
 $solution = "$scriptRoot\..\Sitecore.Modules.PatchableIgnoreList.sln"
 
 & $nuGet restore $solution
 & $msBuild $solution /p:Configuration=Release /t:Rebuild /m
 
-& $nuGet pack "$scriptRoot\..\Sitecore.Modules.PatchableIgnoreList\Sitecore.Modules.PatchableIgnoreList.csproj" -Symbols -Prop Configuration=Release
+& $nuGet pack "$scriptRoot\..\Sitecore.Modules.PatchableIgnoreList\Sitecore.Modules.PatchableIgnoreList.csproj" -Symbols -Prop Configuration=Release -Prop VisualStudioVersion=$vsVersion
